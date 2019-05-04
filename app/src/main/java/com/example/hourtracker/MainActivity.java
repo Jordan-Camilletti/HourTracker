eeb21b00873a275666766bc80ff01d3f85d06cff
Jordan <jordan.camilletti@gmail.com>
2019-05-03 23:55:18 -0500

    Added basic variables

--- app/src/main/java/com/example/hourtracker/MainActivity.java
+++ app/src/main/java/com/example/hourtracker/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private double wage;
+    private float totalOwed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
