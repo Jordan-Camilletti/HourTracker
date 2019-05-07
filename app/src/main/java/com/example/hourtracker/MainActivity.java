cb8c1b3069b3f72d7cc75df3dbf99fe07a38c7b8
Jordan <jordan.camilletti@gmail.com>
2019-05-06 23:42:42 -0500

    Added basic variables

--- app/src/main/java/com/example/hourtracker/MainActivity.java
+++ app/src/main/java/com/example/hourtracker/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.hourtracker;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private double wage;
+    private ConstraintLayout activity_main;
 
+    private double wage;
     private float totalOwed;
     private float paid;
 
