ccfc18320553ca45b3a6e5de70ff2ea69fe02178
Jordan <jordan.camilletti@gmail.com>
2019-05-06 23:47:06 -0500

    Added buttons and associated variables.

--- app/src/main/java/com/example/hourtracker/MainActivity.java
+++ app/src/main/java/com/example/hourtracker/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.hourtracker;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private ConstraintLayout activity_main;
+    private Button addButton;
 
     private double wage;
     private float totalOwed;
