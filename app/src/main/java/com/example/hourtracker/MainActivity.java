59bffba819982534697b2d24b6427a2218e3181d
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-05-08 23:05:32 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/hourtracker/MainActivity.java
+++ app/src/main/java/com/example/hourtracker/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private ConstraintLayout activity_main;
     private Button addButton;
 
