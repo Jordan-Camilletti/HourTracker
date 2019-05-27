786f39a8c03775c63b68f1d129a8bc22507f7438
Jordan <jordan.camilletti@gmail.com>
2019-05-26 21:52:06 -0500

    Added hoursOutput button

--- app/src/main/java/com/example/hourtracker/MainActivity.java
+++ app/src/main/java/com/example/hourtracker/MainActivity.java
@@ -4,32 +4,36 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     private ConstraintLayout activity_main;
+    private TextView hoursOutput;//Output of hours worked and their days
     private Button addButton;
 
-    private float[] dayHours;
-    private float[] dayTimes;
-    private final double wage=12.50;
-    private float totalHours;
+    private float[] days;//Days worked
+    private float[] dayHours;//Hours worked per day
+    private final double wage=12.50;//Wage I'm paid
+    private float totalHours;//What I'm owed
     private float totalOwed;
     private float paid;
 
-    private StringBuilder daysString(float[] hours,float[] times){
+    private StringBuilder daysString(String[] days,float[] hours){
         StringBuilder rtn=new StringBuilder("Start Hours \t Stop Hours \t Hours \t Date\n");
         for(int n=0;n<hours.length;n+=2){
             rtn.append(hours[n]).append("\t");
             rtn.append(hours[n+1]).append("\t");
             rtn.append(hours[n+1]-hours[n]).append("\t");
-            rtn.append(times[n/2]).append("\n");
-}
+            rtn.append(days[n/2]).append("\n");
+        }
         return(rtn);
-                }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hoursOutput=findViewById(R.id.hoursOutput);
     }
 }
