ce17c28ef97c2910e0d294612b5ed8ead2feb574
Jordan <jordan.camilletti@gmail.com>
2019-06-04 01:08:49 -0500

    timeToHours now works properly

--- app/src/main/java/com/example/hourtracker/MainActivity.java
+++ app/src/main/java/com/example/hourtracker/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         //Main is the hour difference between the two
         //Remain is +/- 30 minutes of time to account for times ending at ##:30
         BigDecimal main=BigDecimal.valueOf(Integer.parseInt(hours[index+1].substring(0,2))-Integer.parseInt(hours[index].substring(0,2)));
-        BigDecimal remain=BigDecimal.valueOf((Integer.parseInt(hours[index+1].substring(3))-Integer.parseInt(hours[index].substring(3)))/60);
+        BigDecimal remain=BigDecimal.valueOf((Integer.parseInt(hours[index+1].substring(3))-Integer.parseInt(hours[index].substring(3)))/60.0);
         return(main.add(remain));
     }
 
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         for(int n=0;n<hours.length;n+=2){
             rtn.append(hours[n]).append("\t\t");
             rtn.append(hours[n+1]).append("\t\t");
-            rtn.append(timeToHours(hours,n/2)).append("\t\t");
+            rtn.append(timeToHours(hours,n)).append("\t\t");
             rtn.append(days[n/2]).append("\t\t").append("\n");
         }
         return(rtn);
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
     public BigDecimal getTotalHours(String[] hours){
         BigDecimal sum=new BigDecimal("0");
         for(int n=0;n<hours.length/2;n++){
-            sum=sum.add(timeToHours(hours,n));
+            sum=sum.add(timeToHours(hours,n*2));
         }
         return(sum);
     }
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        System.out.print("X");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
