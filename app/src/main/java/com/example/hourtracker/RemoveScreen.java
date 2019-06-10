65f16905f0015e4d1c758fb1939bdc190438431c
Jordan <jordan.camilletti@gmail.com>
2019-06-10 15:08:49 -0500

    Added .xml files and TODO links to additional .java files

--- app/src/main/java/com/example/hourtracker/RemoveScreen.java
+++ app/src/main/java/com/example/hourtracker/RemoveScreen.java
@@ -1,12 +1,20 @@
 package com.example.hourtracker;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class RemoveScreen extends AppCompatActivity {
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove);
+
+        //https://www.youtube.com/watch?v=3Zrwi3FFrC8
+        mPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor=mPreferences.edit();
     }
 }
