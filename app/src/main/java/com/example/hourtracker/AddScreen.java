c43cbd69362f7d4568e5dc6986bcbe99332469ea
Jordan <jordan.camilletti@gmail.com>
2019-06-11 18:56:43 -0500

    Added some more important stuff

--- app/src/main/java/com/example/hourtracker/AddScreen.java
+++ app/src/main/java/com/example/hourtracker/AddScreen.java
@@ -1,17 +1,23 @@
 package com.example.hourtracker;
 
 import android.content.SharedPreferences;
+import android.inputmethodservice.ExtractEditText;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class AddScreen extends AppCompatActivity {
     private SharedPreferences mPreferences;
     private SharedPreferences.Editor mEditor;
 
+    private EditText wageInput;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        wageInput=(EditText) findViewById(R.id.wageInput);
 
         //https://www.youtube.com/watch?v=3Zrwi3FFrC8
         mPreferences= PreferenceManager.getDefaultSharedPreferences(this);
