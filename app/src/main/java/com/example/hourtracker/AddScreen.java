aa1bd083cc06267ce7f2a3ee2a194d0284f96415
Jordan <jordan.camilletti@gmail.com>
2019-06-12 01:02:40 -0500

    Added back button for wage adding screen

--- app/src/main/java/com/example/hourtracker/AddScreen.java
+++ app/src/main/java/com/example/hourtracker/AddScreen.java
@@ -5,6 +5,8 @@ import android.inputmethodservice.ExtractEditText;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class AddScreen extends AppCompatActivity {
@@ -12,15 +14,24 @@ public class AddScreen extends AppCompatActivity {
     private SharedPreferences.Editor mEditor;
 
     private EditText wageInput;
+    private Button backButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
         wageInput=(EditText) findViewById(R.id.wageInput);
+        backButton=(Button) findViewById(R.id.backButton);
 
         //https://www.youtube.com/watch?v=3Zrwi3FFrC8
         mPreferences= PreferenceManager.getDefaultSharedPreferences(this);
         mEditor=mPreferences.edit();
+
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 }
