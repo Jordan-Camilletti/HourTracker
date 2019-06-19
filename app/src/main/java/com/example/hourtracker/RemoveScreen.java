324f57660bc987e2f35c22cb5aaea2a00697d2ff
Jordan <jordan.camilletti@gmail.com>
2019-06-19 15:16:04 -0500

    Hours now calculated in Remove screen via wage and SharedPreference

--- app/src/main/java/com/example/hourtracker/RemoveScreen.java
+++ app/src/main/java/com/example/hourtracker/RemoveScreen.java
@@ -1,13 +1,16 @@
 package com.example.hourtracker;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.FileOutputStream;
+import java.math.BigDecimal;
 
 public class RemoveScreen extends AppCompatActivity {
 
@@ -16,11 +19,15 @@ public class RemoveScreen extends AppCompatActivity {
     private Button removeButton;
     private EditText paidInput;
 
+    private SharedPreferences mPreferences;
+
+    private BigDecimal hoursPaid=new BigDecimal("0.0");
+    private BigDecimal wage=new BigDecimal("12.50");
     private String FILE_NAME="hours.txt";
 
-    public void resetFile(String FILE_NAME){//Used to reset the contents of "hours.txt" because I'm an idiot
-        try{
-            FileOutputStream fos=openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+    public void resetFile(String FILE_NAME) {//Used to reset the contents of "hours.txt" because I'm an idiot
+        try {
+            FileOutputStream fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
             fos.write("00:00 00:00 0000-00-00 ".getBytes());
             fos.close();
         } catch (Exception e) {
@@ -36,6 +43,8 @@ public class RemoveScreen extends AppCompatActivity {
         clearAllButton=(Button) findViewById(R.id.clearAllButton);
         removeButton=(Button) findViewById(R.id.removeButton);
         paidInput=(EditText) findViewById(R.id.paidInput);
+
+        mPreferences= PreferenceManager.getDefaultSharedPreferences(this);
 
         backButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -54,7 +63,8 @@ public class RemoveScreen extends AppCompatActivity {
         removeButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                wage=new BigDecimal(mPreferences.getString("Wage","12.50"));
+                hoursPaid=new BigDecimal(paidInput.getText().toString()).divide(wage);
             }
         });
     }
