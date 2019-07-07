d6a451f9e56414cd44cf6138b8fd6caeaab62ce7
Jordan <jordan.camilletti@gmail.com>
2019-07-07 00:59:32 -0500

    Removed unused statement

--- app/src/main/java/com/example/hourtracker/RemoveScreen.java
+++ app/src/main/java/com/example/hourtracker/RemoveScreen.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
