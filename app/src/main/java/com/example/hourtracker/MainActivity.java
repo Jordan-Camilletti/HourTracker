a6c15dd71410ceb4d95021d4be2c9f3733b52dd8
Jordan <jordan.camilletti@gmail.com>
2019-07-06 20:40:17 -0500

    Removed unused imports and functions.

--- app/src/main/java/com/example/hourtracker/MainActivity.java
+++ app/src/main/java/com/example/hourtracker/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.hourtracker;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -14,7 +13,6 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
@@ -127,10 +125,22 @@ public class MainActivity extends AppCompatActivity {
         hoursText.setText("Hours:\n");
         datesText.setText("Dates:\n");
         for(int n=2;n<hours.size();n+=2){
-            startHoursText.append(hours.get(n)+"\n");
-            stopHoursText.append(hours.get(n+1)+"\n");
-            hoursText.append(timeToHours(hours,n)+"\n");
-            datesText.append(days.get(n/2)+"\n");
+            if(unpaid.get(n/2).equals(new BigDecimal("0"))){//Totally paid
+                startHoursText.append(hours.get(n)+"\n");
+                stopHoursText.append(hours.get(n+1)+"\n");
+                hoursText.append(timeToHours(hours,n)+"\n");
+                datesText.append(days.get(n/2)+"\n");
+            }else if(unpaid.get(n/2).equals(timeToHours(hours,n))){//Totally unpaid
+                startHoursText.append(hours.get(n)+"\n");
+                stopHoursText.append(hours.get(n+1)+"\n");
+                hoursText.append(timeToHours(hours,n)+"\n");
+                datesText.append(days.get(n/2)+"\n");
+            }else{//Partially paid
+                startHoursText.append(hours.get(n)+"\n");
+                stopHoursText.append(hours.get(n+1)+"\n");
+                hoursText.append(timeToHours(hours,n)+"\n");
+                datesText.append(days.get(n/2)+"\n");
+            }
         }
     }
 
